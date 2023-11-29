17950cbccaffa157c6532d47698984f79b29b08d
TessT1ckles <134611258+TessT1ckles@users.noreply.github.com>
2023-11-29 18:31:00 +0800

    Update viewFile.java

--- viewFile.java
+++ viewFile.java
@@ -3,14 +3,14 @@ package com.mycompany.projectoop;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
 
 class viewFile{
     protected int fileCount;
     protected String date;
-    protected int food, transport, electricity, housing;
-    protected int essential;
-    protected int nonEssential;
+    protected double food, transport, utilityBill;
+    protected double essential;
+    protected double nonEssential;
     
     public viewFile(int fileCount){
         this.fileCount = fileCount;
@@ -21,22 +21,25 @@ class viewFile{
         try(BufferedReader reader = new BufferedReader(new FileReader(fileName))){
             String line;
             date = reader.readLine();
-            food = parseInt(reader.readLine());
-            transport = parseInt(reader.readLine());
-            electricity = parseInt(reader.readLine());
-            housing = parseInt(reader.readLine());
-            essential = parseInt(reader.readLine());
-            nonEssential = parseInt(reader.readLine());
-            System.out.println(date);
-            System.out.println("Food: " + food);
-            System.out.println("Transport: " + transport);
-            System.out.println("Electricity: " + electricity);
-            System.out.println("Housing: " + housing);
-            System.out.println("Total Expenses: " + essential);
-            System.out.println("Other Expenses: " + nonEssential);
+            food = parseDouble(reader.readLine());
+            transport = parseDouble(reader.readLine());
+            utilityBill = parseDouble(reader.readLine());
+            essential = parseDouble(reader.readLine());
+            nonEssential = parseDouble(reader.readLine());
+            System.out.println("\n\n============================");
+            System.out.println(" " + date);
+            System.out.println("============================");
+            System.out.println("   Food          |  " + food);
+            System.out.println("   Transport     |  " + transport);
+            System.out.println("   Utility Bills |  " + utilityBill);
+            System.out.println("----------------------------");
+            System.out.println("   Essential     |  " + essential);
+            System.out.println("   Non-Essential |  " + nonEssential);
+            System.out.println("----------------------------\n");
             reader.close();
         }catch(IOException e){
             System.out.println("Error Reaading File: " + e.getMessage());
         }
     }
 }
+
