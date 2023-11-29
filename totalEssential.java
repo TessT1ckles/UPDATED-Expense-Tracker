b201d53157dddb43478125bf354ef0748c9a6b58
TessT1ckles <134611258+TessT1ckles@users.noreply.github.com>
2023-11-29 19:05:34 +0800

    Update totalEssential.java

--- totalEssential.java
+++ totalEssential.java
@@ -1,15 +1,13 @@
 package com.mycompany.projectoop;
  
     class totalEssential{ 
-        public double add(double value1, double value2){
-            return value1 + value2;
+        public double add(double num1, double num2){
+            return num1 + num2;
         }
         public double add(double num1, double num2, double num3){
             return num1 + num2 + num3;
         }
-        public double subtract(double value1, double value2){
-            return value1 - value2;
+        public double subtract(double num1, double num2){
+            return num1 - num2;
         }
     }
-
-    
